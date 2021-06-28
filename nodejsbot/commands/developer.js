de6838bb07a334e9fd35446e329ec836e6dea26d
siwoo131 <tigersiwoo131@gmail.com>
2021-06-28 10:29:47 +0900

    중간 저장

---
+++ nodejsbot/commands/developer.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "개발자",
+    execute(message){
+        const Discord = require('discord.js');
+        const Embed = new Discord.MessageEmbed()
+            .setTitle("개발자는")
+            .setDescription("바로 미간#8269 (이)야!")
+            .setThumbnail("https://cdn.discordapp.com/avatars/415135882006495242/cb4c1c1fce24f512e07f673989814572.webp?size=1024")
+            .setColor("00FF21")
+            .setFooter("Bot Made by. 미간#8269", "https://cdn.discordapp.com/avatars/415135882006495242/cb4c1c1fce24f512e07f673989814572.webp?size=1024");
+        message.channel.send(Embed);
+    }
+}
--- nodejsbot/commands/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "핑",
-    execute(message){
-        message.reply("퐁!");
-    }
-};
